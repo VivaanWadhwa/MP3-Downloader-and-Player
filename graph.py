238838800d082a594a41ec1019dae53dc974de99
VivaanWadhwa <vivaanw@gmail.com>
2020-05-20 13:04:20 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ import math
 x,y,z=[],[],[]
 for a in range(0,10000,100):
     x.append(a)
-    y.append(math.sin(a))
+    y.append(a)
     
 plt.plot(x, y, color='green', linestyle='dashed', linewidth = 2, 
          marker='o', markerfacecolor='blue', markersize=1)
